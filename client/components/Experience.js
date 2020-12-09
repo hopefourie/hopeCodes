2a1808cde4adaa92cd6810a11217d872b9c640dc
HopeFourie <hopefourie@gmail.com>
2020-12-09 01:04:26 -0500

    update: styling

--- client/components/Experience.js
+++ client/components/Experience.js
@@ -5,33 +5,35 @@ export default function Experience() {
     <div className="component">
       <div className="experiences">
         <h2>Experience</h2>
-        <div className="experience">
-          <p className="experience-dates">Sept 2020-Dec 2020</p>
-          <div className="experience-titles">
-            <h3>Full Stack Engineer In-Training</h3>
-            <h2>Grace Hopper Program at Fullstack Academy</h2>
+        <div className="experience-list">
+          <div className="experience1">
+            <div className="experience-titles">
+              <h3>Full Stack Engineer In-Training</h3>
+              <h4>Grace Hopper Program at Fullstack Academy</h4>
+            </div>
+            <p className="experience-dates">Sept 2020-Dec 2020</p>
           </div>
-        </div>
-        <div className="experience">
-          <div className="experience-titles">
-            <h3>Costume Designer</h3>
-            <h2>Dating (York Avenue Productions)</h2>
+          <div className="experience2">
+            <div className="experience-titles">
+              <h3>Costume Designer</h3>
+              <h4>Dating (York Avenue Productions)</h4>
+            </div>
+            <p className="experience-dates">Oct 2019-Nov 2019</p>
           </div>
-          <p className="experience-dates">Oct 2019-Nov 2019</p>
-        </div>
-        <div className="experience">
-          <p className="experience-dates">Aug 2019-Jan 2020</p>
-          <div className="experience-titles">
-            <h3>Costume Production Assistant</h3>
-            <h2>Tommy (CBS)</h2>
+          <div className="experience1">
+            <div className="experience-titles">
+              <h3>Costume Production Assistant</h3>
+              <h4>Tommy (CBS)</h4>
+            </div>
+            <p className="experience-dates">Aug 2019-Jan 2020</p>
           </div>
-        </div>
-        <div className="experience">
-          <div className="experience-titles">
-            <h3>Costume Shop Supervisor</h3>
-            <h2>Wesleyan University</h2>
+          <div className="experience2">
+            <div className="experience-titles">
+              <h3>Costume Shop Supervisor</h3>
+              <h4>Wesleyan University</h4>
+            </div>
+            <p className="experience-dates">Sept 2015-May 2019</p>
           </div>
-          <p className="experience-dates">Sept 2015-May 2019</p>
         </div>
       </div>
     </div>
--- client/components/Projects.js
+++ client/components/Projects.js
@@ -5,51 +5,53 @@ export default function Projects() {
     <div className="component">
       <div className="projects">
         <h2>Projects</h2>
-        <div className="project">
-          <div className="project-text">
-            <h3>RegEx Spaceship</h3>
-            <p>
-              A web-based multiplayer game that tests players' knowledge of
-              Regular Expression syntax while exploring a 2D spaceship. With a
-              base of Node.js and HTML, my team and I implemented socket.io to
-              create and manage web sockets, allowing players to interact with
-              each other in real time. We also used Express and PostgreSQL to
-              organize multiple simultaneous games within a database.
-            </p>
+        <div className="projects-list">
+          <div className="project">
+            <div className="project-text">
+              <h3>RegEx Spaceship</h3>
+              <p>
+                A web-based multiplayer game that tests players' knowledge of
+                Regular Expression syntax while exploring a 2D spaceship. With a
+                base of Node.js and HTML, my team and I implemented socket.io to
+                create and manage web sockets, allowing players to interact with
+                each other in real time. We also used Express and PostgreSQL to
+                organize multiple simultaneous games within a database.
+              </p>
+            </div>
+            {/* add img path */}
+            <img />
           </div>
-          {/* add img path */}
-          <img />
-        </div>
-        <div className="project">
-          {/* add img path */}
-          <img />
-          <div className="project-text">
-            <h3>M+J Noodle Emporium</h3>
-            <p>
-              A mock e-commerce site where users can log in, browse a selection
-              of noodles, add to their cart, and checkout their selected
-              products. With a base of Node.js, HTML5, and CSS, I implemented
-              Google OAuth to allow users to sign in using their Google
-              credentials. I also used React, Redux, Express, and PostgreSQL to
-              create and manage the checkout component, ensuring that the user
-              sessions and database are updated in real time.
-            </p>
+          <div className="project">
+            {/* add img path */}
+            <img />
+            <div className="project-text">
+              <h3>M+J Noodle Emporium</h3>
+              <p>
+                A mock e-commerce site where users can log in, browse a
+                selection of noodles, add to their cart, and checkout their
+                selected products. With a base of Node.js, HTML5, and CSS, I
+                implemented Google OAuth to allow users to sign in using their
+                Google credentials. I also used React, Redux, Express, and
+                PostgreSQL to create and manage the checkout component, ensuring
+                that the user sessions and database are updated in real time.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="project">
-          <div className="project-text">
-            <h3>Newt's Quest</h3>
-            <p>
-              A web-based platform game in which players use keyboard controls
-              to venture throughout a magical 2D world as Newt the gnome,
-              fighting goblins and collecting fireflies. With a base of Node.js
-              and HTML, I implemented the Phaser 3 API to design a multi-scene
-              game. I also used Express, PostgreSQL and Redux to allow players
-              to submit scores to a database.
-            </p>
+          <div className="project">
+            <div className="project-text">
+              <h3>Newt's Quest</h3>
+              <p>
+                A web-based platform game in which players use keyboard controls
+                to venture throughout a magical 2D world as Newt the gnome,
+                fighting goblins and collecting fireflies. With a base of
+                Node.js and HTML, I implemented the Phaser 3 API to design a
+                multi-scene game. I also used Express, PostgreSQL and Redux to
+                allow players to submit scores to a database.
+              </p>
+            </div>
+            {/* add img path */}
+            <img />
           </div>
-          {/* add img path */}
-          <img />
         </div>
       </div>
     </div>
