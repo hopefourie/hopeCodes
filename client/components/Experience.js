54f3d24e6188d5d96e24ff374026d7954a9c49ac
HopeFourie <hopefourie@gmail.com>
2020-12-09 10:50:08 -0500

    update: add styling

--- client/components/Experience.js
+++ client/components/Experience.js
@@ -7,28 +7,31 @@ export default function Experience() {
         <h2>Experience</h2>
         <div className="experience-list">
           <div className="experience1">
-            <div className="experience-titles">
+            <div className="experience-text">
               <h3>Full Stack Engineer In-Training</h3>
               <h4>Grace Hopper Program at Fullstack Academy</h4>
+              <p className="experience-dates">Sept 2020-Dec 2020</p>
             </div>
-            <p className="experience-dates">Sept 2020-Dec 2020</p>
+            <img className="experience-img" src="./graceHopper.jpg"/>
           </div>
           <div className="experience2">
-            <div className="experience-titles">
+          <img className="experience-img" src="./nbc.png"/>
+            <div className="experience-text">
               <h3>Costume Designer</h3>
               <h4>Dating (York Avenue Productions)</h4>
+              <p className="experience-dates">Oct 2019-Nov 2019</p>
             </div>
-            <p className="experience-dates">Oct 2019-Nov 2019</p>
+
           </div>
           <div className="experience1">
-            <div className="experience-titles">
+            <div className="experience-text">
               <h3>Costume Production Assistant</h3>
               <h4>Tommy (CBS)</h4>
             </div>
             <p className="experience-dates">Aug 2019-Jan 2020</p>
           </div>
           <div className="experience2">
-            <div className="experience-titles">
+            <div className="experience-text">
               <h3>Costume Shop Supervisor</h3>
               <h4>Wesleyan University</h4>
             </div>
