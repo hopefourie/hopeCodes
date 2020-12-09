2bed4842c7abb57ab68b9e2a95c7d6b298f825fe
HopeFourie <hopefourie@gmail.com>
2020-12-09 11:20:07 -0500

    update: styling

--- client/components/Experience.js
+++ client/components/Experience.js
@@ -15,27 +15,28 @@ export default function Experience() {
             <img className="experience-img" src="./graceHopper.jpg"/>
           </div>
           <div className="experience2">
-          <img className="experience-img" src="./nbc.png"/>
+            <img className="experience-img" src="./nbc.png"/>
             <div className="experience-text">
               <h3>Costume Designer</h3>
               <h4>Dating (York Avenue Productions)</h4>
               <p className="experience-dates">Oct 2019-Nov 2019</p>
             </div>
-
           </div>
           <div className="experience1">
             <div className="experience-text">
               <h3>Costume Production Assistant</h3>
               <h4>Tommy (CBS)</h4>
+              <p className="experience-dates">Aug 2019-Jan 2020</p>
             </div>
-            <p className="experience-dates">Aug 2019-Jan 2020</p>
+            <img className="experience-img" src="./cbs.png"/>
           </div>
           <div className="experience2">
+            <img className="experience-img" src="./wesleyan.png"/>
             <div className="experience-text">
               <h3>Costume Shop Supervisor</h3>
               <h4>Wesleyan University</h4>
+              <p className="experience-dates">Sept 2015-May 2019</p>
             </div>
-            <p className="experience-dates">Sept 2015-May 2019</p>
           </div>
         </div>
       </div>
