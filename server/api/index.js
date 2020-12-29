247cbb76e28a23b6653d01b3aa48811aee86ef55
HopeFourie <hopefourie@gmail.com>
2020-12-29 15:52:39 -0500

    update: removed unused api routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-router.use('/users', require('./users'));
-
 router.use((req, res, next) => {
   const err = new Error('Not found.');
   err.status = 404;
